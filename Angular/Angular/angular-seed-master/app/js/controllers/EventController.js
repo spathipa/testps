ac7783290a9ebb60e07981adc5101f1ec95e4510
unknown <Krishna Pathipaka@KPATH63307.USGA.org>
2013-10-24 12:25:42 -0400

    adding files

---
+++ Angular/Angular/angular-seed-master/app/js/controllers/EventController.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+eventsApp.controller('EventController', function EventController($scope) {
+
+    $scope.event = {
+        name: 'Angular Boot Camp',
+        date: '10/21/2013',
+        time: '2:41 pm',
+        location: {
+            address:'Google Headquarters',
+            city:'Mountain View',
+            province:'CA'   
+        },
+        sessions: [
+       {
+           name:'Directives Masterclass'
+       },
+       {
+           name: 'Scope for fun and Profit'
+       },
+       {
+           name: 'Well Behaved controller'
+       }
+                  ]
+    }
+
+   
+        }
+    
+    );
