238524026441a0dd178349b7684ecdc9e649ac46
unknown <Krishna Pathipaka@KPATH63307.USGA.org>
2013-10-24 14:09:44 -0400

    adding files

---
+++ AngularBreeze/AngularBreeze/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace AngularBreeze.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
